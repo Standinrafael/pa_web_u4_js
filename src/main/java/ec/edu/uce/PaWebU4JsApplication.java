3b85c05887fc2e79319b8fdff6e5991d0d2e7d9d
Jorge Sanchez <jlsanchez1997@hotmail.es>
2022-03-18 20:49:23 -0500

    Taller 39: Parallel Stream

---
+++ src/main/java/ec/edu/uce/PaWebU4JsApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaWebU4JsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaWebU4JsApplication.class, args);
+	}
+
+}
